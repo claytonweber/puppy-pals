424007c5b7d2c5030add7300b5062094baab49ba
claytonweber <claytronweber@gmail.com>
2023-05-31 12:57:35 -0600

    puppy mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { puppyList } from './data.js';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  
   return (
     <>
       <div>
-        
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
